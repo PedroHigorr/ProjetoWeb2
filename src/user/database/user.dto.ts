b34e37ab6e972ef6e45f22d75884719208dcf6c1
PRISCILA <cg3024229@labA205m38.IFSP>
2024-11-13 22:27:13 -0300

    teste 3

---
+++ src/user/database/user.dto.ts
@@ -0,0 +1,26 @@
+export interface CadastroUsuario {
+    name: string,
+    email: string,
+    tel: string
+}
+
+export interface DadosUsuario{
+    id: number,
+    name: string,
+    email: string,
+    tel: string | null,
+    createdAt: Date
+}
+
+export interface UsuarioDto{
+
+    criarUsuario(dados: CadastroUsuario)
+
+    lerDados(dados: DadosUsuario)
+
+    atualizarDados(dados: CadastroUsuario)
+
+    deletarUsuario(id: number)
+
+    
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { UsuarioDto } from './database/user.dto';
+import { CadastroUsuario, DadosUsuario } from './database/user.dto';import { PrismaClient } from '@prisma/client';
+
 
 @Injectable()
-export class UserService {}
+export class UserService implements UsuarioDto {
+   
+    private prisma = new PrismaClient();
+
+    criarUsuario(dados: CadastroUsuario){
+
+        const variavel = 2
+    }
+
+    lerDados(dados: DadosUsuario){}
+
+    atualizarDados(dados: CadastroUsuario){}
+
+    deletarUsuario(id: number){}
+
+    funcaoTeste(id: number ) {
+dsadasads
+    }
+}
